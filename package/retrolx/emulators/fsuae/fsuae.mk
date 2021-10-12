cf0926407d807e58634d8bf1a88cd774c5cae090
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-12 08:25:42 +0200

    WIP experiment with FS-UAE on arm/aarch64, gles(2) support is not here yet

--- package/retrolx/emulators/fsuae/fsuae.mk
+++ package/retrolx/emulators/fsuae/fsuae.mk
@@ -3,11 +3,23 @@
 # FSUAE
 #
 ################################################################################
-# Version.: Commits on Jun 11, 2021 (4.0.x)
-FSUAE_VERSION = 80f746286d9a9347ab8252a7cb7c45df3e887fd2
+# Version.: Commits on Oct 10, 2021 (4.0.x)
+FSUAE_VERSION = f00b12db92ea40a87edb2d0ffae92e8d8196479c
 FSUAE_SITE = $(call github,FrodeSolheim,fs-uae,$(FSUAE_VERSION))
 FSUAE_LICENSE = GPLv2
-FSUAE_DEPENDENCIES = xserver_xorg-server openal libpng sdl2 zlib libmpeg2 libglib2 libcapsimage
+FSUAE_DEPENDENCIES = openal libpng sdl2 zlib libmpeg2 libglib2 libcapsimage
+
+ifeq ($(BR2_x86_64)$(BR2_i686),y)
+FSUAE_DEPENDENCIES += xserver_xorg-server
+else ifeq ($(BR2_arm),y)
+FSUAE_CONF_OPTS += --disable-jit --without-opengl
+#FSUAE_CONF_ENV  += OPENGL_LIBS="-lEGL -lGLESv2"
+FSUAE_CONF_ENV += CFLAGS="-DSUPPORT_XINPUT2=0"
+else ifeq($(BR2_aarch64),y)
+FSUAE_CONF_OPTS += --disable-jit --without-opengl
+#FSUAE_CONF_ENV  += OPENGL_LIBS="-lEGL -lGLESv2"
+FSUAE_CONF_ENV += CFLAGS="-DSUPPORT_XINPUT2=0"
+endif
 
 FSUAE_CONF_OPTS += --disable-codegen
 
